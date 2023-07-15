e34831044b14911d51062eccc7fd39c73ac8dd36
Alex <alexgeorgegeer@gmail.com>
2023-07-15 03:57:48 -0500

    first draft posts page and nav complete

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,15 @@
 import '../styles/styles.css';
+import NavStopScroll from './modules/NavStopScroll.js';
+
+let navStopScroll = new NavStopScroll();
+
+
+// let checkBox = document.querySelector('.navigation__checkbox');
+// checkBox.addEventListener('click', () => {
+//   document.querySelector('body').classList.toggle('stop-scroll');
+// });
+
+
 
 if (module.hot) {
   module.hot.accept();
